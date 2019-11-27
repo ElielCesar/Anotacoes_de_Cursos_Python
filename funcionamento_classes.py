0b3dd13d3b8abc02ac576bc2774cebad3c04ad43
Eliel Cesar da Silva Lima <eliel.tecinfo@gmail.com>
2019-11-27 08:50:42 -0400

    Notas de aulas sobre Classes em POO

---
+++ funcionamento_classes.py
@@ -0,0 +1,139 @@
+'''
+Classe = Estrutura maior
+Instancia = permite acessar recursos da classe
+Obejeto = A variavel que recebera uma classe.
+atributo = variaveis internas da classe
+metodo = funcoes que existem dentro de uma classe
+'''
+
+# criando uma classe 
+class Classe:
+    def metodo(self):
+        print('teste')
+
+# instanciando a classe
+variavel = Classe()
+# chamando um metodo da classe
+variavel.metodo()
+
+'''------------------------------------------------------------'''
+
+# criando classes - 02
+class Classe1:
+    def metodo1(self):
+        print('Metodo da classe 1')
+
+class Classe2:
+    def metodo2(self):
+        print('metodo da classe 2')
+
+# chamando a classe e seus metodos diretamente sem 
+# instanciar um objeto para isso.
+Classe1().metodo1()
+Classe2().metodo2()
+
+'''------------------------------------------------------------'''
+
+# criando classes 03
+class Mensagens:
+    def saudacao(self):
+        print('Seja bem vindo!')
+
+    def despedida(self):
+        print('ate logo!')
+
+
+Mensagens().saudacao()
+Mensagens().despedida()
+
+'''------------------------------------------------------------'''
+
+# varias formas de declarar uma classe.
+
+# forma mais utilizada
+class Classe1:
+    def metodo(self):
+        print('classe 1')
+
+# segunda forma mais utilizada
+class Classe2():
+    def metodo(self):
+        print('classe 2')
+
+# menos utilizada 
+class Classe3(object):
+    def metodo(self):
+        print('classe 3')
+        
+'''------------------------------------------------------------'''
+
+'''
+Quando o script .py roda, o interpretador carrega na memoria(ponteiro)
+todas as classes existentes no arquivo, depois disso ele 
+somente vai usar a classe se ela for referenciada em algum lugar
+no codigo.
+
+Resumindo: primeiro o interpretador python lê todo o codigo e 
+depois volta do inicio executando linha por linha.
+'''
+
+# metodo construtor - 01
+class Classe:
+    ''' 
+    o metodo construtor sera executado automaticamente toda
+    vez que instanciarmos a classe '''
+
+    def __init__(self):
+        print('classe carregada em memoria...')
+
+
+#Classe()
+
+'''------------------------------------------------------------'''
+
+# metodo construtor - 02
+class Classe2:
+    ''' 
+    o metodo construtor sera executado automaticamente toda
+    vez que instanciarmos a classe '''
+
+    def __init__(self):
+        print('classe carregada em memoria...')
+
+    def metodo(self):
+        print('metodo acionado...')
+
+'''Vai chamar o construtor automaticamente e tambem o metodo'''
+#Classe2().metodo()
+
+'''------------------------------------------------------------'''
+
+# metodo construtor - 03
+class Classe3:
+    def __init__(self, msg):
+        print(msg)
+
+Classe3('Eu passei essa mensagem')
+
+'''
+Veja que eu passei a mensagem e ela foi chamada automaticamente
+sem que eu precisa-se invocar nenhum metodo, pois o construtor 
+se autoexecuta toda vez que a classe eh instanciada.
+Nesse caso o parametro msg obrigatoriamente tem que ser passado 
+ao executarmos a classe.
+'''
+
+# da pra usar o construtor e o metodo juntos
+class Classe:
+    
+    def __init__(self, msg):
+        print(msg)
+
+    def metodo(self):
+        print('metodo acionado...')
+
+Classe('Aprendendo POO em Python').metodo()
+
+'''------------------------------------------------------------'''
+
+
