c7802bdce569f40776a177e434c1cd7f4344646c
Eliel Cesar da Silva Lima <eliel.tecinfo@gmail.com>
2019-11-28 12:19:18 -0400

    Update funcionamento_metodos.py

--- funcionamento_metodos.py
+++ funcionamento_metodos.py
@@ -60,6 +60,63 @@ f.metodo(msg2='mensagem 2', msg1='mensagem 1')
 '''____________________________________________________________________________'''
 
 
+# Exemplo 5
+'''É possivel chamar um metodo a partir de outro na mesma classe'''
+class Classe5:
+    def metodo1(self):
+        print('metodo 1')
+
+    def metodo2(self):
+        self.metodo1()
+        print('metodo 2 ')
+
+g = Classe5()
+g.metodo2()
+
+'''____________________________________________________________________________'''
+
+# Exemplo 6
+'''E possivel chamar um metodo de uma outra classe apenas instanciando-a'''
+class Classe6:
+    def metodo(self):
+        print('metodo da classe 6')
+
+
+class Classe7:
+    def metodo7(self):
+        print('metodo da classe 7')
+
+    def metodo_principal(self):
+        Classe6().metodo() # veja que aqui a Classe6 foi instanciada.
+        self.metodo7() # aqui eu chamo o metodo da propria Classe7 usando self
+
+
+c7 = Classe7()
+c7.metodo_principal()
+
+'''____________________________________________________________________________'''
+
+# usando metodos em cascata
+
+'''Nesse exemplo o metodo2 recebe msg1 e joga para o metodo1, 
+o metodo2 recebe msg2 e imprime na tela.
+
+Resumindo: A ideia de cascata é um método jogando parametros para 
+outro metodo na mesma classe.
+'''
+
+class Classe8:
+    def metodo1(self, msg1):
+        print(msg1.upper()) # transforma em maiuscula apenas para fins didaticos
+
+    def metodo2(self, msg1, msg2):
+        self.metodo1(msg1)
+        print(msg2)
+
+
+k = Classe8()
+k.metodo2('mensagem 1', 'mensagem 2')
+
 
 
 
