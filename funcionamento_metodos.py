ade8217d09d0a063cda1c10fb697d40d400197c3
Eliel Cesar da Silva Lima <eliel.tecinfo@gmail.com>
2019-11-28 11:21:01 -0400

    Update funcionamento_metodos.py

--- funcionamento_metodos.py
+++ funcionamento_metodos.py
@@ -28,7 +28,7 @@ del d
 '''Uma classe que nao instanciar seu construtor somente
 podera utilizar o metodo se algum valor for passado como
 parametro no lugar do self, ex: metodo(self)'''
-
+# Exemplo 3
 class Classe3:
     def __init__(self):
         print('construtor executado')
@@ -42,13 +42,19 @@ class Classe3:
 
 Classe3.metodo(None) # nesse caso passamos o None mas poderia ser outro valor.
 
+'''____________________________________________________________________________'''
 
+'''Podemos passar 2 ou mais argumentos para o metodo sem problemas '''
+# Exemplo 04
+class Classe4:
+    def metodo(self, msg1, msg2):
+        print(msg1)
+        print(msg2)
 
+f = Classe4()
+f.metodo('Estou aprendendo python', 'Eu sou um hacker')
 
-
-
-
-
+'''____________________________________________________________________________'''
 
 
 
