aba50e98bc3c86e8509c6316c3d272109aaf53ac
Eliel Cesar da Silva Lima <eliel.tecinfo@gmail.com>
2019-12-02 23:23:55 -0400

    Update funcionamento_excessoes.py

--- funcionamento_excessoes.py
+++ funcionamento_excessoes.py
@@ -17,7 +17,7 @@ class Divisao2:
     def dividir(self, n1=0, n2=0):
         try:
             print(f'{n1} / {n2} = {n1/n2}')
-        except Exception as erro:
+        except Exception as erro: # esse trecho pega o erro original
             print(erro)
 
 d = Divisao2()
@@ -27,8 +27,28 @@ d.dividir(10, 0)
 
 '''______________________________________________________________'''
 
+# eh possivel criar algumas excessoes personalizadas para um caso
+# especifico usando o raise
+class Divisao3:
+    def dividir(self, n1=0, n2=0):
+        try:
+            if n2 == 'a': # se a condicao for verdadeira gera uma exception personalizada.
+                raise Exception('O segundo argumento nao eh inteiro')
+            print(f'{n1} / {n2} = {n1/n2}')
+
+        except Exception as erro:
+            print(erro)
 
 
+d = Divisao3()
+d.dividir(10, 2)
+d.dividir(10, 'a')
+d.dividir(10, 0)
 
+'''______________________________________________________________'''
 
+# para ignorar o erro do except simplimente adicione um pass ao final.
+
+except:
+    pass
 
