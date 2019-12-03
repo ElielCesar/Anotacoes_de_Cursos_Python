cfd57c3428006d2bc56fe6bd21f679dc5e4b637f
Eliel Cesar da Silva Lima <eliel.tecinfo@gmail.com>
2019-12-03 17:16:36 -0400

    Update funcionamento_heranca.py

--- funcionamento_heranca.py
+++ funcionamento_heranca.py
@@ -24,3 +24,35 @@ print(c2.atributo1) # veja que a Classe2 acessa os atributos da classe1
 print(c2.atributo2)
 c2.metodo1()        # veja que a Classe2 acessa os metodos da classe1
 c2.metodo2()
+
+'''
+OBS: Quando a classe pai e a classe filha tiverem os mesmos nomes de atributos, métodos ou funcoes
+os metodos da classe filha vao sobrescrever os da classe pai, dessa forma preste atencao aos nomes
+destes quando estiver programando.
+'''
+
+
+# utilizando herancas multiplas em cascata
+
+class Classe1:
+    def metodo1(self):
+        print('Metodo da Classe1')
+
+class Classe2(Classe1): # herda da Classe1
+    def metodo2(self):
+        print('Metodo da Classe2')
+
+class Classe3(Classe2): # herda da Classe2
+    def metodo3(self):
+        print('Metodo da Classe3')
+
+class Classe4(Classe3):# herda da Classe3
+    def metodo4(self):
+        print('Metodo da Classe4')
+
+
+c4 = Classe4() # Acessa atributos e metodos de Todas as classes devido a heranca multipla.
+c4.metodo1()
+c4.metodo2()
+c4.metodo3()
+
