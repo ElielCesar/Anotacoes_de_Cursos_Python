d909532bdd045335d3e5bef0b079387bf5319dc5
Eliel Cesar da Silva Lima <eliel.tecinfo@gmail.com>
2019-12-18 19:18:59 -0400

    Update poo_alura_parte1.py

--- poo_alura_parte1.py
+++ poo_alura_parte1.py
@@ -1,25 +1,25 @@
 class Conta:
-    #construtor e atributos
+    # construtor e atributos
     def __init__(self, numero, titular, saldo, limite):
         self.__numero = numero
         self._titular = titular
         self.__saldo = saldo
         self.__limite = limite
 
-    #metodo
+    # metodo
     def depositar(self, valor=0):
         self.__saldo += valor
         print(f'Seu Novo Saldo eh de: {self.__saldo}\n')
 
-    #metodo
+    # metodo
     def sacar(self, valor=0):
         if (self.__saldo - valor) < 0:
             print(f'Saldo Insuficiente para realizar esse saque!\n')
         else:
             self.__saldo -= valor
             print(f'Seu Novo Saldo eh de: {self.__saldo}\n')
-    
-    #metodo
+
+    # metodo
     def extrato(self):
         print('Dados Bancarios:')
         print(f'Conta: {self.__numero}')
@@ -27,18 +27,31 @@ class Conta:
         print(f'Saldo: {self.__saldo}')
         print(f'Limite: {self.__limite}\n')
 
-#Instanciando objetos
+    # metodo
+    def transferir(self, destino, valor_transf=0):
+        # veja que o self vale para o objeto instanciado atual
+        self.sacar(valor_transf) 
+        destino.depositar(valor_transf)
+        # veja que o self vale para o objeto instanciado atual
+        self.extrato()
+        destino.extrato()
+
+
+# Instanciando objetos
 c1 = Conta(1, 'Eliel Cesar', 3000, 1000)
 c2 = Conta(2, 'Renilce', 5000, 1000)
 c3 = Conta(3, 'Aline', 1500, 1000)
 
-#Chamando os metodos
-c3.depositar()
-c3.sacar()
+# Chamando os metodos
+#c3.depositar()
+#c3.sacar()
 
 # Listando extratos
-c1.extrato()
-c2.extrato()
-c3.extrato()
+#c1.extrato()
+#c2.extrato()
+#c3.extrato()
+
+# Transferindo valores
+c1.transferir(c3, 1400)
 
 
