e4eb68b581ab39945707a482d6c9ed6798d59b80
Eliel Cesar da Silva Lima <eliel.tecinfo@gmail.com>
2019-12-18 13:50:58 -0400

    Create poo_alura_parte1.py

---
+++ poo_alura_parte1.py
@@ -0,0 +1,44 @@
+class Conta:
+    #construtor e atributos
+    def __init__(self, numero, titular, saldo, limite):
+        self.__numero = numero
+        self._titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    #metodo
+    def depositar(self, valor=0):
+        self.__saldo += valor
+        print(f'Seu Novo Saldo eh de: {self.__saldo}\n')
+
+    #metodo
+    def sacar(self, valor=0):
+        if (self.__saldo - valor) < 0:
+            print(f'Saldo Insuficiente para realizar esse saque!\n')
+        else:
+            self.__saldo -= valor
+            print(f'Seu Novo Saldo eh de: {self.__saldo}\n')
+    
+    #metodo
+    def extrato(self):
+        print('Dados Bancarios:')
+        print(f'Conta: {self.__numero}')
+        print(f'Titular: {self._titular}')
+        print(f'Saldo: {self.__saldo}')
+        print(f'Limite: {self.__limite}\n')
+
+#Instanciando objetos
+c1 = Conta(1, 'Eliel Cesar', 3000, 1000)
+c2 = Conta(2, 'Renilce', 5000, 1000)
+c3 = Conta(3, 'Aline', 1500, 1000)
+
+#Chamando os metodos
+c3.depositar()
+c3.sacar()
+
+# Listando extratos
+c1.extrato()
+c2.extrato()
+c3.extrato()
+
+
